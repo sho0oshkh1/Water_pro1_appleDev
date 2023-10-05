005af237d1a55e4bc470df932965d0fa0f075a20
shoug <sho0oshkh@gmail.com>
2023-10-05 12:01:55 +0300

    Initial Commit

---
+++ Water_pro1_appleDev/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Water_pro1_appleDev
+//
+//  Created by shoug on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Water_pro1_appleDev/Water_pro1_appleDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_pro1_appleDevApp.swift
+//  Water_pro1_appleDev
+//
+//  Created by shoug on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Water_pro1_appleDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
